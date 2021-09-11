91cf6818df1e941ddc87f77d78b458c12eb9961e
Joseph <JosephSchander@gmail.com>
2021-09-11 18:45:22 -0400

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,23 @@
+package org.example;
+
+
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+public class App {
+
+    public static void main(String[] args) {
+        DecimalFormat DF = new DecimalFormat("##.##");
+        Scanner uInput = new Scanner(System.in);
+        System.out.println("What is the order amount?");
+        double OrderAmount = uInput.nextDouble();
+        uInput.nextLine();
+        double Tax = .055 * OrderAmount;
+        System.out.println("What is the state?");
+        String State = uInput.nextLine();
+        System.out.println("The subtotal is $" + DF.format(OrderAmount));
+        if (State.equals("WI") == true) {
+            System.out.println("The tax is: $" + Tax + "\nThe total is: $" + DF.format(OrderAmount + Tax));
+        }
+    }
+}
